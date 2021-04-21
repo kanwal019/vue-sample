0905006daa912d3707d6c12af72fe00d84590d6f
kanwal019 <kanwal019@gmail.com>
2021-04-22 02:10:26 +0530

    Added more events

--- app.js
+++ app.js
@@ -4,15 +4,23 @@ const app = Vue.createApp({
             title: 'Template for the component !!',
             username: 'John Smith',
             userid: 911,
-            isDisplayed: true
+            isDisplayed: true,
+            mouseX: 0,
+            mouseY: 0,
         }
     },
     methods: {
-        changeTitle(title) {            
-            typeof(title) !== 'string' ? this.title = 'Changed Title !!' : this.title = title
+        changeTitle(title) {
+            typeof (title) !== 'string' ? this.title = 'Changed Title !!' : this.title = title
         },
         toggleDisplay() {
             this.isDisplayed = !this.isDisplayed
+        },
+        handleEvent(event) {
+            console.log(event);
+            console.log(event.type);
+            this.mouseX = event.offsetX;
+            this.mouseY = event.offsetY;
         }
     }
 })
