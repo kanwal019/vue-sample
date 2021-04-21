876b05aba07a2e30c72d977a71fdb1c47d5d7634
kanwal019 <kanwal019@gmail.com>
2021-04-22 02:43:39 +0530

    Use of conditional classes

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ const app = Vue.createApp({
             mouseX: 0,
             mouseY: 0,
             data: [
-                { name: 'John Smith', age: 30, gender: 'male', image: 'https://randomuser.me/api/portraits/men/1.jpg' },
-                { name: 'John Doe', age: 40, gender: 'male', image: 'https://randomuser.me/api/portraits/men/2.jpg' },
-                { name: 'Jane Smith', age: 40, gender: 'female', image: 'https://randomuser.me/api/portraits/women/3.jpg' },
-                { name: 'Jane Doe', age: 30, gender: 'female', image: 'https://randomuser.me/api/portraits/women/4.jpg' },
-                { name: 'Jon Smith', age: 50, gender: 'male', image: 'https://randomuser.me/api/portraits/men/5.jpg' },
-                { name: 'Joan Smith', age: 40, gender: 'female', image: 'https://randomuser.me/api/portraits/women/6.jpg' },
-                { name: 'Job Smith', age: 30, gender: 'male', image: 'https://randomuser.me/api/portraits/men/7.jpg' },
+                { name: 'John Smith', age: 30, gender: 'male', image: 'https://randomuser.me/api/portraits/men/1.jpg', isFamous: true },
+                { name: 'John Doe', age: 40, gender: 'male', image: 'https://randomuser.me/api/portraits/men/2.jpg', isFamous: false },
+                { name: 'Jane Smith', age: 40, gender: 'female', image: 'https://randomuser.me/api/portraits/women/3.jpg', isFamous: false  },
+                { name: 'Jane Doe', age: 30, gender: 'female', image: 'https://randomuser.me/api/portraits/women/4.jpg', isFamous: false  },
+                { name: 'Jon Smith', age: 50, gender: 'male', image: 'https://randomuser.me/api/portraits/men/5.jpg', isFamous: true  },
+                { name: 'Joan Smith', age: 40, gender: 'female', image: 'https://randomuser.me/api/portraits/women/6.jpg', isFamous: false  },
+                { name: 'Job Smith', age: 30, gender: 'male', image: 'https://randomuser.me/api/portraits/men/7.jpg', isFamous: true  },
             ]
         }
     },
