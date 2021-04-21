c33fffd5cedaef16c2f4eb86f903d38190f78a45
kanwal019 <kanwal019@gmail.com>
2021-04-21 13:45:20 +0530

    Added a conditional

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const app = Vue.createApp({
         return {
             title: 'Template for the component !!',
             username: 'John Smith',
-            userid: 911
+            userid: 911,
+            isDisplayed: true
         }
     },
     methods: {
         changeTitle(title) {            
             typeof(title) !== 'string' ? this.title = 'Changed Title !!' : this.title = title
+        },
+        toggleDisplay() {
+            this.isDisplayed = !this.isDisplayed
         }
     }
 })
